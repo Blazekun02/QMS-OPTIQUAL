767d70ee513f0813006a57fd68a18915be44b3ce
Yllanexs <149398171+Yllanexs@users.noreply.github.com>
2025-04-23 11:02:42 +0800

    Add files via upload

---
+++ QADSpecificComponents/QADMain/QAD-POV.js
@@ -0,0 +1,57 @@
+const sidebar = document.querySelector('.Sidebar');
+const hamburgerIcon = document.getElementById('hamburger-icon');
+
+    sidebar.addEventListener('mouseenter', () => {
+        sidebar.classList.add('extended');
+        hamburgerIcon.style.left = '2.5in';
+    });
+
+    sidebar.addEventListener('mouseleave', () => {
+        sidebar.classList.remove('extended');
+        hamburgerIcon.style.left = '0.8in';
+    });
+    hamburgerIcon.addEventListener('click', () => {
+        const isExtended = greyLine.classList.toggle('extended');
+        hamburgerIcon.style.left = isExtended ? '2.5in' : '0.8in';
+    });
+
+    const notifButton = document.getElementById('notifButton');
+    const notificationOverlay = document.getElementById('popupOverlay');
+
+    notifButton.addEventListener('click', () => {
+        if (notificationOverlay.style.display === 'block') {
+            notificationOverlay.style.display = 'none';
+        } else {
+            notificationOverlay.style.display = 'block';
+        }
+    });
+   
+
+    
+    const signOutOverlay = document.getElementById('signOutOverlay');
+    const userButton = document.getElementById('userButton');
+    userButton.addEventListener('click', () => {
+        signOutOverlay.style.display = signOutOverlay.style.display === 'block' ? 'none' : 'block';
+    });
+
+    document.getElementById("signOutOverlay").addEventListener("click", function () {
+        window.location.href = "landingPage.html";
+    });
+
+    
+
+// Role Manager
+
+function showDepartmentManager() {
+    const departmentPanel = document.querySelector('.Department-Manager-Panel');
+    
+    // Toggle visibility
+    if (departmentPanel.style.display === 'none' || departmentPanel.style.display === '') {
+        departmentPanel.style.display = 'block';
+    } else {
+        departmentPanel.style.display = 'none';
+    }
+}
+
+// Attach the function to the sidebar menu item
+document.querySelector('.menu-icons:nth-child(6)').addEventListener('click', showDepartmentManager);
